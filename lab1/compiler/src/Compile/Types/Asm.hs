22fb8a7cdfc00d838b154394cc634b7919f3af62
John Cole <jhbcole@gmail.com>
2013-09-11 13:29:45 -0400

    Added Register type for the x86-64 registers.

--- lab1/compiler/src/Compile/Types/Asm.hs
+++ lab1/compiler/src/Compile/Types/Asm.hs
@@ -8,5 +8,22 @@ data Asm = AsmRet
          | Movl Arg Arg 
          | Addl Arg Arg 
          | Subl Arg Arg deriving Show
-data Arg = Reg ALoc
+
+data Arg = Reg Register
          | Val Int deriving Show
+
+data Register = EAX
+              | EBX
+              | ECX
+              | EDX
+              | ESI
+              | EDI
+              | ESP
+              | EBP
+              | R9
+              | R10
+              | R11
+              | R12
+              | R13
+              | R14
+              | R15
