99a354a691e34f7a9d38105eaf8386cbf22ac41f
Kevin Burg <kburg@andrew.cmu.edu>
2013-10-12 11:50:27 -0400

    Fix underscores for linux.

--- lab3/compiler/l3rt.c
+++ lab3/compiler/l3rt.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-extern int c0_main();
+extern int _c0_main();
 
 /* The main function, which calls _c0_main */
 int main() {
-  printf("%d\n", c0_main());
+  printf("%d\n", _c0_main());
   exit(0);
 }
