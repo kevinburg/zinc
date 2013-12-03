c5965d9f87be6598cd27a68fe51ea72003bca6d0
Kevin Burg <kburg@andrew.cmu.edu>
2013-12-03 12:43:46 -0500

    Report.

---
+++ lab6/report/PolyExample.c
@@ -0,0 +1,16 @@
+struct foo <a> {
+  int size;
+  a[] array;
+};
+
+int getSize(struct foo <a> *s) {
+  return s->size;
+}
+
+int main() {
+  int[] A = alloc_array(int, 5);
+  struct foo <int> *s = alloc(struct foo <int>);
+  s->size = 5;
+  s->array = A;
+  return 0;
+}
