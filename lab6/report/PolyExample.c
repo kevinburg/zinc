72a3ed35aa4e916c92e1ef719ec7cbb0984a2d31
Kevin Burg <kburg@andrew.cmu.edu>
2013-12-05 10:26:28 -0500

    Working on the specification.

--- lab6/report/PolyExample.c
+++ lab6/report/PolyExample.c
@@ -3,11 +3,14 @@ struct foo <a> {
   a[] array;
 };
 
+int getSize(struct foo <a> *s) {
+  return s->size;
+}
+
 int main() {
   int[] A = alloc_array(int, 5);
   struct foo <int> *s = alloc(struct foo <int>);
   s->size = 5;
-  A[3] = 3;
   s->array = A;
-  return 0;
+  return getSize(s);
 }
